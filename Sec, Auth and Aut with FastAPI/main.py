87100fc2758f7d8cce2948c81d493e6e3b925ccf
Vitor Dallagnolo <vitor.dallagnolo@avitaseg.com.br>
2022-12-13 16:51:47 -0300

    Removed the secret of main file

--- Sec, Auth and Aut with FastAPI/main.py
+++ Sec, Auth and Aut with FastAPI/main.py
@@ -13,11 +13,3 @@ if __name__ == '__main__':
 
     uvicorn.run("main:app", host="0.0.0.0", port=8000,
                 log_level='info', reload=True)
-
-
-"""
-Token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNjZXNzX3Rva2VuIiwiZXhwIjoxNjUyNDQyMTE4LCJpYXQiOjE2NTE4MzczMTgsInN1YiI6IjIifQ.5yeW2y_M3eAYrv-369FLsTTkjAFQn6W_eUR19Ivz_YA
-Tipo: bearer
-
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNjZXNzX3Rva2VuIiwiZXhwIjoxNjUyNDQzNDk1LCJpYXQiOjE2NTE4Mzg2OTUsInN1YiI6IjMifQ.jTq0xkcILa7kgrtMJhcew6OIwXODEjX24CzCToY7bbU
-"""
